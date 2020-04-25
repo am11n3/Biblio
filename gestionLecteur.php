1022790a9eef661ba4de3aca8f4bacbe83d9aed9
am11n3 <62472417+am11n3@users.noreply.github.com>
2020-04-25 00:18:34 +0000

    Add files via upload

--- gestionLecteur.php
+++ gestionLecteur.php
@@ -1,23 +1,18 @@
 <?php
 session_start();
-$vide = 0;	$done = 0;
 	try {
-	$conn = new PDO('mysql:host=localhost;dbname=librairie', 'root', '');
-	} 
-	catch (PDOException $e) 
+	$conn = new PDO('mysql:host=localhost;dbname=librairie', 'root', '');	
+
+	$vide = 0;
+	$done = 0;
+ 
+	if(!isset($_SESSION['numL']))
 	{
-	    print "Erreur !: " . $e->getMessage() . "<br/>";
-	    die();
-	}
+		$nom = $_POST['nom_lecteur'];
+		$mot_de_passe = $_POST['password'];
 
+		echo '<h1>Gestion du lecteur</h1>';
 
- if(!isset($_SESSION['numL']))// Si lecteur authentifier 
-     {
-     	if(	isset($_POST['login']))
-		{
-			$nom = $_POST['nom_lecteur'];
-			$mot_de_passe = $_POST['password'];
-     	
 		if($nom && $mot_de_passe)
 		{
 			$req = $conn->prepare("SELECT * FROM lecteurs WHERE lecnom = ? AND lecmotdepasse = ?"); 
@@ -25,53 +20,52 @@ $vide = 0;	$done = 0;
 			$userexist = $req->rowCount();
 			if ($userexist == 1) 
 			{
-				 echo '<h1>Gestion du lecteur</h1>';
 				$userinfo = $req->fetch();
 				$_SESSION['numL']= $userinfo['lecnum'];
 				$done=1;
 			}
 			else
 			{
-				$erreur = "Le lecteur n'est pas reconnu" .'<br>Cliquez <a href="login.php">ici</a> pour tenter une nouvelle identification';
+				echo "Le lecteur n'est pas reconnu" .'<br>Cliquez <a href="login.php">ici</a> pour tenter une nouvelle identification';
 				$vide = 1;
 			}
 		}
 		else
 		{
-			$erreur = '<h3 style="color:red;"> Veuillez remplir tous les champs </h3>'.
+			echo '<h3 style="color:red;"> Veuillez remplir tous les champs </h3>'.
 			'<a href="login.php">Retourner a la page d\'authentification </a>';
-		}}
-		if(isset($erreur))
-		{	echo $erreur;	}
+		}
+		if( $valide==1)
+  	     {include'LectReconnu.php';} 
+	}
+	else
+	{
+		echo '<h1>Gestion du lecteur</h1>';
 
+	  	 $tab_code=explode("/",$_SESSION['livreCode']);
+	  	 for($i=1;$i<count($tab_code);$i++)
+	  	 {
+	  	 	$sql3="SELECT * from  livres where livcode ='$tab_code[$i]'";
+	  	 	$result=$conn->query($sql3);
+	  	 	$Tab=$result->fetch();
+	        echo"<tr>
+	        		<td>$Tab[0]</td>
+	        		<td>$Tab[1]</td>
+	        		<td>$Tab[2]</td>
+	        		<td>$Tab[3]</td>
+	        		<td>$Tab[4]</td>
+	        		<td>$Tab[5]</td>
+	        	</tr>";
+	        $result->closeCursor();
 
-			if($vide==1) require 'LecReconnu.php';
-  	         
-  	  }
-  	  else
-      {             
-      	echo '<h1>Gestion du lecteur</h1>';
-  	   	$tab_code=explode("/",$_SESSION['livreCode']);
-  	     	include 'LectReconnu.php';
-  	        for($i=1;$i<count($tab_code);$i++)
-  	     	 {     
- 	     	     $sql3="SELECT * from  livres where livcode ='$tab_code[$i]'";
-                     $result=$conn->query($sql3);
+	  	 }
+	  	 echo"</table>";
 
-                     $T1=$result->fetch();
-                     echo"<tr>
-		        		<td>$Tab[0]</td>
-		        		<td>$Tab[1]</td>
-		        		<td>$Tab[2]</td>
-		        		<td>$Tab[3]</td>
-		        		<td>$Tab[4]</td>
-		        		<td>$Tab[5]</td>
-		        	</tr>";
-                     $result->closeCursor();
-                  }
-               echo"</table>";         
-       }
-   
-?>
-</body>
-</html>
+	}
+
+	}
+	catch (PDOException $e) 
+	{
+	    print "Erreur !: " . $e->getMessage() . "<br/>";
+	    die();
+	}
